8ee9d05e9f9bebf3579daf9e8c70617d8d2a8b04
Matt Taylor <matt.taylr22@gmail.com>
2020-03-02 22:17:45 +0000

    updated tail.js to properly separate array elements to check

--- tail.js
+++ tail.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
-  if (Array.isArray(actual)) {
-    actual = actual.toString();
-  }
-  if (Array.isArray(expected)) {
-    expected = expected.toString();
-  }
+  // if (Array.isArray(actual)) {
+  //   actual = actual.toString();
+  // }
+  // if (Array.isArray(expected)) {
+  //   expected = expected.toString();
+  // }
   (actual === expected) ? console.log(`😇😇😇 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
 };
 
@@ -13,10 +13,19 @@ assertEqual(1, 1);
 assertEqual(2, "2");
 
 const tail = function(arr) {
-  let newArray = arr;
+  let newArray = arr.slice();
   newArray.shift();
   return newArray;
 };
 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+assertEqual(result[0], words[1]);
+assertEqual(result[1], words[2]);
+
+
+
