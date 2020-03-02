d648d366fbbc43c91f909d1a8cc98d574a7833d6
Matt Taylor <matt.taylr22@gmail.com>
2020-03-02 20:00:37 +0000

    console.assert replacement function

---
+++ assertEqual.js
@@ -0,0 +1,6 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log("😇😇😇" + " Assertion Passed: " + actual + " === " + expected) : console.log("😡😡😡" + " Assertion Failed: " + actual + " !== " + expected);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
