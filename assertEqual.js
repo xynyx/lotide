3d4518e9e8e4b37b31329527f06256bdd689433b
Matt Taylor <matt.taylr22@gmail.com>
2020-03-02 20:20:30 +0000

    updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,7 @@
 const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log("😇😇😇" + " Assertion Passed: " + actual + " === " + expected) : console.log("😡😡😡" + " Assertion Failed: " + actual + " !== " + expected);
+  (actual === expected) ? console.log(`😇😇😇 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(2, "2");
