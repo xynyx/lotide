d36293609058b9def71dfab68ddcd6dfc6332e33
Matt Taylor <matt.taylr22@gmail.com>
2020-03-05 21:23:01 +0000

    delete array when callback is true

---
+++ takeUntil.js
@@ -0,0 +1,35 @@
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  array.forEach(function(el, index) {
+    if (callback(el)) {
+      newArray = array.slice(0, index);
+    }
+  })
+  return newArray;
+};
+
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`😇😇😇 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+};  
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length !== array2.length) { 
+      return false;
+    }
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
