ffcd716e325472e85e1160f7847f508694480c02
Matt Taylor <matt.taylr22@gmail.com>
2020-03-07 00:27:53 +0000

    eqObject in recursion

---
+++ eqObjectsRecursion.js
@@ -0,0 +1,20 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
