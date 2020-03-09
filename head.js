052e26b7a4374bf59530757b58910776e550292b
Matt Taylor <matt.taylr22@gmail.com>
2020-03-09 13:29:28 +0000

    refactored and made test files for head.js and assertEqual.js

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, "2");
-
 const head = arr => arr.shift();
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;
